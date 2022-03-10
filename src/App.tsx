2519a593c6818b9d1ac5d4a2ec2dd36e64e01625
chinwendu <conuoha@infinion.co>
2022-03-10 14:15:17 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss';
+import loading from './assets/loader.gif'
+import { AiOutlineSearch } from 'react-icons/ai'
+
 
 function App() {
+  const [news, setNews] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [filteredNews, setFilteredNews] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => {
+        setNews(response.data);
+      })
+  }, [])
+
+  const handleSearchInput = (searchValue:any) => {
+    setSearchInput(searchValue.target.value)
+    
+  }
+
+  const resetInputFiled = () => {
+    setSearchInput('')
+  }
+
+  const searchNews = (event:any) => {
+    event.preventDefault();
+      let result = news.filter((item) => {
+        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+      })
+      setFilteredNews(result)
+      resetInputFiled();
+    }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app-header'>
+        <div className='search-container'>
+           <AiOutlineSearch className='icon'/>
+            <form className = "form" onSubmit={searchNews} >
+              <input 
+                type="text" 
+                className='text-input' 
+                placeholder='Search candidate...' 
+                value={searchInput}
+                onChange={handleSearchInput}
+              />
+              
+                <button className='search-button' > Search</button>
+            
+            </form>
+           
+        </div>
+      </div>
+      
+        {filteredNews.map((item:any) => {
+            return ( 
+              <div className='card-container'>
+                <div className='card1'>
+                  <h1 className = "head">{item.name}
+                        <span className = "span">
+                          {item.title}
+                        </span>
+                    </h1>
+                </div>
+              
+                <div className='card2'>
+                <h1 className = "head">{item.name}
+                        <span className = "span">
+                          {item.title}
+                        </span>
+                    </h1>
+                </div>
+      
+                <div className='card3'>
+                <h1 className = "head">{item.name}
+                        <span className = "span">
+                          {item.title}
+                        </span>
+                    </h1>
+                </div>
+              </div>
+            )
+          }) 
+        }
+        
+              
     </div>
   );
 }
